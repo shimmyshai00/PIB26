060703f4553292502f09342e2ce6997545108aed
Shimrra Shai <shimmyshai00@gmail.com>
2021-04-01 22:44:29 -0700

    remove extraneous files from debug folder

--- PIB26/Debug/src/util/subdir.mk
+++ PIB26/Debug/src/util/subdir.mk
@@ -4,16 +4,25 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../src/util/DotsTicker.cpp \
 ../src/util/LabelTicker.cpp \
-../src/util/timer.cpp 
+../src/util/printB26.cpp \
+../src/util/timer.cpp \
+../src/util/userInput.cpp 
 
 OBJS += \
+./src/util/DotsTicker.o \
 ./src/util/LabelTicker.o \
-./src/util/timer.o 
+./src/util/printB26.o \
+./src/util/timer.o \
+./src/util/userInput.o 
 
 CPP_DEPS += \
+./src/util/DotsTicker.d \
 ./src/util/LabelTicker.d \
-./src/util/timer.d 
+./src/util/printB26.d \
+./src/util/timer.d \
+./src/util/userInput.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
