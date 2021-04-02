0a6f80db89ef0f0610de4cd16bc553c2fc1bcbf1
Shimrra Shai <shimmyshai00@gmail.com>
2021-04-01 22:46:51 -0700

    update build folders

--- PIB26/Release/src/bignum/multiplication/FFT/complex/subdir.mk
+++ PIB26/Release/src/bignum/multiplication/FFT/complex/subdir.mk
@@ -8,21 +8,27 @@ CPP_SRCS += \
 ../src/bignum/multiplication/FFT/complex/genOmegaTable.cpp \
 ../src/bignum/multiplication/FFT/complex/rad2Itr.cpp \
 ../src/bignum/multiplication/FFT/complex/rad2Rec.cpp \
-../src/bignum/multiplication/FFT/complex/rad3Rec.cpp 
+../src/bignum/multiplication/FFT/complex/rad3Rec.cpp \
+../src/bignum/multiplication/FFT/complex/rad4Itr.cpp \
+../src/bignum/multiplication/FFT/complex/rad4Rec.cpp 
 
 OBJS += \
 ./src/bignum/multiplication/FFT/complex/ComplexFft.o \
 ./src/bignum/multiplication/FFT/complex/genOmegaTable.o \
 ./src/bignum/multiplication/FFT/complex/rad2Itr.o \
 ./src/bignum/multiplication/FFT/complex/rad2Rec.o \
-./src/bignum/multiplication/FFT/complex/rad3Rec.o 
+./src/bignum/multiplication/FFT/complex/rad3Rec.o \
+./src/bignum/multiplication/FFT/complex/rad4Itr.o \
+./src/bignum/multiplication/FFT/complex/rad4Rec.o 
 
 CPP_DEPS += \
 ./src/bignum/multiplication/FFT/complex/ComplexFft.d \
 ./src/bignum/multiplication/FFT/complex/genOmegaTable.d \
 ./src/bignum/multiplication/FFT/complex/rad2Itr.d \
 ./src/bignum/multiplication/FFT/complex/rad2Rec.d \
-./src/bignum/multiplication/FFT/complex/rad3Rec.d 
+./src/bignum/multiplication/FFT/complex/rad3Rec.d \
+./src/bignum/multiplication/FFT/complex/rad4Itr.d \
+./src/bignum/multiplication/FFT/complex/rad4Rec.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
